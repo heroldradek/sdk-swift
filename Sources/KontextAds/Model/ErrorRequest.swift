975031b7be37ab16cc4f1d4ad96d207f2f8f59d8
Radek Herold <>
2025-08-05 23:02:15 +0200

    improvements

--- Sources/KontextAds/Model/ErrorRequest.swift
+++ Sources/KontextAds/Model/ErrorRequest.swift
@@ -1,16 +1,16 @@
 import Foundation
 
-public struct ErrorRequest: Codable {
+public struct ErrorRequest: Codable, Sendable {
     let error: String
-    let additionalData: AdditionalData
+    let additionalData: AdditionalData?
 
-    public init(error: String, additionalData: AdditionalData) {
+    public init(error: String, additionalData: AdditionalData?) {
         self.error = error
         self.additionalData = additionalData
     }
 }
 
-public struct AdditionalData: Codable {
+public struct AdditionalData: Codable, Sendable {
     let preloadBodyRequest: PreloadBodyRequest?
 
     public init(preloadBodyRequest: PreloadBodyRequest?) {
